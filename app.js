8af694f7da2109bed4159d13d9d2edc4a7d6122d
Dipankar GHOSH <dipankarghosh2003@gmail.com>
2020-10-31 21:00:01 +0530

    Add files via upload
    Updated files

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 var tappedCardDeck=0;
 var dropinvalidcard=0;
+var basePath="assets/";
+var correctCard=basePath+"card-4-hearts.png";
+var card4Club=basePath+"card-4-clubs.png";
+var card4Diamond=basePath+"card-4-diamond.png";
+var card4Spade=basePath+"card-4-spade.png";
+var invalidCard=basePath+"card-Q-spade.png";
+var cardDeck=basePath+"card-deck-back.png";
+var currency=basePath+"notes-100.png";
+var phoneScreen=basePath+"phone-screen-new.png";
+var confetti=basePath+"confetti.png";
 $(document).ready(function(){
+    $("#correctcard img").attr("src",correctCard);
+    $("#topc1 img").attr("src",card4Club);
+    $("#topc2 img").attr("src",card4Diamond)
+    $("#topc3 img").attr("src",invalidCard)
+    $("#topc4 img").attr("src",card4Spade);
+    $("#correctcard img").attr("src",correctCard);
+    $("#invalidcard img").attr("src",invalidCard);
+    $(".front img").attr("src",cardDeck);
+    $(".back img").attr("src",correctCard);
+    $("#currency img").attr("src",currency);
+    $("#phoneScreen img").attr("src",phoneScreen);
+    $("#confettiContainer img").attr("src",confetti);
+
     $("#game-area .msg").animate({opacity:1},500);
+    
     setTimeout(interoanima,1500);
     
     $("#pileofcard").bind("click",function(){
@@ -40,8 +64,6 @@ $(document).ready(function(){
 
 function interoanima(){
     $("#game-area #pileofcard").animate({left:"71px",top:"7px"},1000,function(){
-       //$("#game-area .msg").html("Make Sequence, Make Money");
-        
        animatecardopen();
     });
     setTimeout(tapToPick,1200)
@@ -101,7 +123,7 @@ function flip() {
     $('#game-area .carddef').addClass('flipped');;
     $("#game-area #showhand").css({left:'182px'});
     setTimeout(function(){
-        $("#game-area .msg").html("Tap to Complete the Sequence");
+        $("#game-area .msg").html("Tap to Complete the Set");
         tappedCardDeck=2;
         $("#card1").css({opacity:"0"});
         $("#correctcard").css({opacity:"1"});
@@ -183,55 +205,41 @@ function deleteWrongCard(){
 function nextAnim(){
    
     setTimeout(function(){
-        $("#game-area .msg").html("Sequence Complete");
+        $("#game-area .msg").html("Set Complete");
     },500)
     
     
     setTimeout(function(){
-        //$("#game-area #confetti3").addClass("animateConfetti")
         $("#game-area .msg").html("");
         $('#game-area #topc3').hide()
-       // $('#game-area #topc4').css({transform:"rotate(14deg)",left:"184px",top:"15px",width:"84px"});
-       // $("#game-area #correctcard").hide();
         $("#game-area #pileofcard").hide()
         $("#game-area #invalidcard").hide();
         
         $("#game-area #correctcard").animate({top:"200px"},1000)
         $("#game-area #bottomcardcontainer").animate({top:"200px"},1000,showFinal)
-    },1500);
-    setTimeout(function(){
-        //uplifeCards();
-    },2200)
+    },2000);
+    
 }
 
-function uplifeCards(){
-    $("#game-area #pileofcard").animate({opacity:"0"},400);
-    $("#game-area #invalidcard").animate({opacity:"0"},400,function(){
-        $('#game-area #topc4').css({transform:"rotate(14deg)",left:"175px",top:"15px",opacity:"1",width:"84px"});
-        $('#game-area #topc4 img').attr("src","./assets/card-4-hearts.png");
-        $("#game-area #correctcard").hide();
-        $("#game-area #bottomcardcontainer").animate({top:"27px"},1000,showFinal)
-    })
-}
 
 function showFinal(){
     $("#game-area .msg").html("Congratulations!");
     $("#game-area .msg").css({color:"#cab991"});
     $("#game-area .msg2").css({display:"block"});
     $("#game-area .msg2").animate({opacity:"1"},500);
-    $("#game-area #money").animate({top:"223px"},500);
+    $("#game-area #currencyContainer").animate({top:"223px"},500);
     setTimeout(function(){
-        $("#game-area #confetti3").addClass("animateConfetti1");
+        $("#game-area #confettiContainer").addClass("animateConfetti1");
     },1000)
     
-    // $("#game-area .msg2").show();
+   
     $("#game-area #bottomcardcontainer").hide();
-    $("#game-area #money").show();
+    $("#game-area #currencyContainer").show();
     setTimeout(function(){
-        $("#game-area #money").animate({opacity:"0"},1000);
+        $("#game-area #currencyContainer").animate({opacity:"0"},1000);
         $("#game-area .msg2").animate({opacity:"0"},1000);
         $("#game-area .msg").animate({opacity:"0"},1000);
-        $("#game-area #confetti3").removeClass("animateConfetti1");
+        $("#game-area #confettiContainer").removeClass("animateConfetti1");
        setTimeout(function(){
         nfinal();
        },1200)
@@ -241,12 +249,12 @@ function showFinal(){
 
 function nfinal(){
 
-    $("#game-area #phone").delay(200).animate({top:"0%"},1000);
+    $("#game-area #phoneScreen").delay(200).animate({top:"0%"},1000);
     $("#game-area .msg").css({color:"#ffffff"});
     $("#game-area .msg").html("Play More, Earn More");
     $("#game-area .msg").css({"font-size":"24px"});
     $("#game-area .msg").delay(200).animate({opacity:"1"},1000);
-    $("#game-area #confetti3").removeClass("animateConfetti1");
+    $("#game-area #confettiContainer").removeClass("animateConfetti1");
     setTimeout(function(){
         nxfinal();
     },2400)
@@ -255,9 +263,9 @@ function nfinal(){
 function nxfinal(){
     $("#game-area .msg").css({"font-size":"18px"});
     $("#game-area .msg").html("Guinness Record  for  World&#39;s <br/> Largest Tournament");
-    $("#game-area #confetti3").addClass("animateConfetti1");
+    $("#game-area #confettiContainer").addClass("animateConfetti1");
     setTimeout(function(){
-        $("#game-area #confetti3").hide();
+        $("#game-area #confettiContainer").hide();
     },1000)
 }
 
